cc83fa2eaf60fd00544236c3ce862ff6188b2ef8
basto34 <bastien.thirion@eleves.ec-nantes.fr>
2023-11-23 09:34:37 +0100

    Initial commit

---
+++ Bataille_Navale/Board.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<vector>
+using namespace std;
+
+class Board
+{
+private:
+	vector<vector<int>> grille;
+	int largeur;
+	int longueur;
+	int nbBateaux;
+public:
+	bool estVide();
+	Board(int lar, int lon);
+
+};
+
